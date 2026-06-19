aa528c168289266924b6966819107ede195c6fb6
Kurs270 <109516250+Kurs270@users.noreply.github.com>
2026-06-19 09:00:31 +0200

    Remove pkg_resources (copied from victron2mqtt) update mini_html to V 0.18.1 remove obsolete mini_html parameter "do_not_minify_doctype"

--- tools/mini_html.py
+++ tools/mini_html.py
@@ -1,28 +1,34 @@
 Import("env")
 import os
 import glob
+from importlib.metadata import PackageNotFoundError, version
 from pathlib import Path
 import sys
-import pip
 import subprocess
-import pkg_resources
 
 def ensure_module_version(package_name, required_version):
     try:
-        installed_version = pkg_resources.get_distribution(package_name).version
+        installed_version = version(package_name)
         if installed_version == required_version:
             print(f"{package_name} {required_version} is already installed.")
             return
         else:
             print(f"{package_name} version {installed_version} found – replacing with {required_version}.")
-    except pkg_resources.DistributionNotFound:
+    except PackageNotFoundError:
         print(f"{package_name} is not installed – installing version {required_version}.")
 
     # Install the required version (will upgrade or downgrade as needed)
-    subprocess.check_call(['pip', 'install', '--upgrade', f'{package_name}=={required_version}'])
+    subprocess.check_call([
+        sys.executable,
+        '-m',
+        'pip',
+        'install',
+        '--upgrade',
+        f'{package_name}=={required_version}',
+    ])
 
 
-ensure_module_version("minify_html", "0.15.0")
+ensure_module_version("minify_html", "0.18.1")
 import minify_html
 
 filePath = 'src/webpages/'
@@ -47,7 +53,8 @@ try:
         cpp_output += tmp  
    else:
        #tmp = f.read()  #disable compression until fixed that the compressor remove %VARIABLE%
-       tmp = minify_html.minify(f.read(),do_not_minify_doctype=True, minify_js=True)
+       #tmp = minify_html.minify(f.read(),do_not_minify_doctype=True, minify_js=True)
+       tmp = minify_html.minify(f.read(), minify_js=True)
        contentlength = len(tmp)
        cpp_output += tmp
 
